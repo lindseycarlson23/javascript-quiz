8b70bec2b841fbce8b8774f5e9d16b227dc5679f
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-10-04 15:32:40 -0500

    fixed timer functionality

--- Assets/script.js
+++ Assets/script.js
@@ -31,16 +31,25 @@ var currentQuestion = 0;
 
 var questionElement = document.querySelector('.question');
 var optionsElement = document.querySelector('.options');
+var score = 0;
+var secondsLeft;
 
 
+
+function endGame() {
+    //runs if you run out of time or finish questions
+    //provide input for initials
+    var userInitials = document.createElement("INPUT");
+    userInitials.setAttribute("type", "text");
+    document.querySelector("#question-container").innerHTML="";
+    document.querySelector("#question-container").appendChild(userInitials);
+    //show high scores
+}
 
 
 
 function loadQuestion() {
-    // console.log("The loop is started: ", index);
-    // console.log("Current state of index after start: ", index);
-    // setTime();
-    
+      
     var currentQuizData = quizData[currentQuestion];
     questionElement.textContent = currentQuizData.question;
     optionsElement.innerHTML = '';
@@ -50,20 +59,23 @@ function loadQuestion() {
         button.textContent = option;
         button.addEventListener('click', () => {
             if (index !== currentQuizData.answer) { //if the answer is incorrect
-                //subtract 15 seconds  
+                secondsLeft -=10;
+                console.log("wrongAnswer")
                 currentQuestion++; 
                 
                 //and add one point
                 if (currentQuestion < quizData.length) {
                         loadQuestion();
                 } else { 
-                        questionElement.textContent = "Quiz completed"; //end the quiz
-                        optionsElement.innerHTML = '';
+                        endGame();
+                        // questionElement.textContent = "Quiz completed"; //end the quiz SHOW HIGH SCORES
+                        // optionsElement.innerHTML = '';
                 }; 
             // correct answer, move to the next question
             } else {
+                score+=1;
                 currentQuestion++; 
-                //and add one point
+                
                 if (currentQuestion < quizData.length) {
                         loadQuestion();
                 } else { 
@@ -74,32 +86,46 @@ function loadQuestion() {
         });
         optionsElement.appendChild(button);
     });
-}
+};
 
 // loadQuestion();
     
 function startQuiz() {
-    var secondsLeft = 30;
-    secondsLeft = document.getElementById("time").innerHTML;
+    console.log("I am here!")
+    secondsLeft = 30;
+    var timer = document.getElementById("time");
     document.getElementById("startButton").classList.add("hidden");
     document.getElementById("question-container").classList.remove("hidden");
+    document.getElementById("time").classList.remove("hidden");
     var timerInterval = setInterval(function() {
         secondsLeft--;
-        // timeElem.textContent = secondsLeft + " seconds left";
-        secondsLeft = document.getElementById("time").innerHTML;
+        timer.textContent = secondsLeft + " seconds left";
+        // secondsLeft = document.getElementById("time").innerHTML;
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
+
             // showScores();
-        }
+        };
     }, 1000);
     loadQuestion();
 };
+
+function wrongAnswer() {
+    secondsLeft -=10;
+}
+
 
 document
     .getElementById("startButton")
     .addEventListener("click", startQuiz);
 
+
+
+//TODO
+//End Game function
+//Set score and get score functions - this is how to store on local storage
+//optional - reset game
 
 // Timer!!!
 // var secondsLeft = 30;
