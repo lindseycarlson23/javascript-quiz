36a4fb6c8346dc9ec95d1b3c7fe3ff19029243f1
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-10-04 21:59:24 -0500

    added styling and README content

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,29 @@
-
+//question array
 var quizData = [
     {
         question: "Which is the correct way to write a comment in JavaScript?",
-        options: ["??...", "//...", "{#...#}", "!---....---!"], //options - put each option on a button
-        answer: 1 //index
+        options: ["??...", "//...", "{#...#}", "!---....---!"], 
+        answer: 1 
     },
     {
-        question: "What is my fave number?",
-        options: [1, 2, 3, 4], //options - put each option on a button
-        answer: 2 //index
+        question: "Which array method sorts the elements of an array?",
+        options: ["changeOrder(order)", "order()", "sort()", "goBanadas()"], 
+        answer: 2 
     },
     {
-        question: "What is my fave food?",
-        options: [1, 2, 3, 4], //options - put each option on a button
-        answer: 2 //index
+        question: "How do you create a new function in JavaScript?",
+        options: ["new.function() {}", "function myFunction() {}", "function = myFunction() {}", "function:myFunction() {}"], //options - put each option on a button
+        answer: 2 
     },
     {
-        question: "What is my fave person?",
-        options: [1, 2, 3, 4], //options - put each option on a button
-        answer: 1 //index
+        question: "Which is not a primitive data type in JavaScript?",
+        options: ["String", "Object", "Boolean", "Numbers"], 
+        answer: 1 
     },
     {
-        question: "What is my fave book?",
-        options: [1, 2, 3, 4], //options - put each option on a button
-        answer: 3 //index
+        question: "What is the actual value supplied when a function is called?",
+        options: ["array", "trueValue", "null", "argument"], 
+        answer: 3 
     }
 ];
 
@@ -34,19 +34,21 @@ var optionsElement = document.querySelector('.options');
 var score = 0;
 var secondsLeft;
 
-
-
+//runs if you run out of time or finish questions
+//creates input box for initials
+//displays score
 function endGame() {
-    //runs if you run out of time or finish questions
-    //provide input for initials
     var userInitials = document.createElement("INPUT");
     userInitials.setAttribute("type", "text");
+    userInitials.classList.add("initials");
     document.querySelector("#question-container").innerHTML="";
     document.querySelector("#question-container").appendChild(userInitials);
     var saveInitials = document.createElement("button");
     saveInitials.textContent = "Save Initials";
+   
     document.querySelector("#question-container").appendChild(saveInitials);
-    saveInitials.addEventListener('click', function(){
+    saveInitials.addEventListener('click', function(event){
+        event.preventDefault();
         let initialsAndScore = {
             initials: userInitials.value,
             score: score
@@ -54,48 +56,49 @@ function endGame() {
         localStorage.setItem("Initials", JSON.stringify(initialsAndScore)) ;
         document.querySelector("#question-container").innerHTML="";
         let initials = JSON.parse(localStorage.getItem("Initials"));
-        document.querySelector("#question-container").textContent="Initials: " + initials.initials + " score: " + initials.score;
+        document.querySelector("#question-container").innerHTML= "initials: " + initials.initials + "<br/>" + "score: " + initials.score;
     });
-
-
-    //show high scores
 };
 
 
-
-function loadQuestion() {
-      
+//advances through the questions and reduces timer as applicable
+function loadQuestion() {  
     var currentQuizData = quizData[currentQuestion];
     questionElement.textContent = currentQuizData.question;
     optionsElement.innerHTML = '';
 
     currentQuizData.options.forEach((option, index) => {
         var button = document.createElement('button');
+        button.classList.add("answer-buttons");
         button.textContent = option;
         button.addEventListener('click', () => {
             if (index !== currentQuizData.answer) { //if the answer is incorrect
-                secondsLeft -=10;
-                console.log("wrongAnswer")
+                secondsLeft -=10; 
                 currentQuestion++; 
-                
-                //and add one point
+                if (secondsLeft <= 0) { //ends game is timer goes to or past 0
+                    document.getElementById("time").classList.add("hidden");
+                    endGame();
+                }
+                                
                 if (currentQuestion < quizData.length) {
                         loadQuestion();
                 } else { 
-                        questionElement.textContent = "This is a test"; //end the quiz SHOW HIGH SCORES
+                        questionElement.textContent = "This is a test"; 
                         optionsElement.innerHTML = '';
+                        document.getElementById("time").classList.add("hidden");
                         endGame();
                 }; 
-            // correct answer, move to the next question
-            } else {
+          
+            } else { //if the answer is correct
                 score+=1;
                 currentQuestion++; 
                 
-                if (currentQuestion < quizData.length) {
+                if (currentQuestion < quizData.length) { //if there are more questions
                         loadQuestion();
-                } else { 
-                        questionElement.textContent = "This is a test"; //end the quiz
+                } else { //if there aren't more questions
+                        questionElement.textContent = "This is a test";
                         optionsElement.innerHTML = '';
+                        document.getElementById("time").classList.add("hidden");
                         endGame();
                 }; 
             };
@@ -107,7 +110,6 @@ function loadQuestion() {
 
     
 function startQuiz() {
-    console.log("I am here!")
     secondsLeft = 30;
     var timer = document.getElementById("time");
     document.getElementById("startButton").classList.add("hidden");
@@ -120,6 +122,7 @@ function startQuiz() {
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
+            document.getElementById("time").classList.add("hidden");
             endGame();
             // showScores();
         };
@@ -127,9 +130,7 @@ function startQuiz() {
     loadQuestion();
 };
 
-// function wrongAnswer() {
-//     secondsLeft -=10;
-// }
+
 
 
 document
@@ -137,29 +138,3 @@ document
     .addEventListener("click", startQuiz);
 
 
-
-//TODO
-//End Game function
-//Set score and get score functions - this is how to store on local storage
-//optional - reset game
-
-// Timer!!!
-// var secondsLeft = 30;
-// secondsLeft = document.getElementById("time").innerHTML;
-
-// function setTime() {
-//     var timerInterval = setInterval(function() {
-//         secondsLeft--;
-//         // timeElem.textContent = secondsLeft + " seconds left";
-//         secondsLeft = document.getElementById("time").innerHTML;
-
-//         if(secondsLeft === 0) {
-//             clearInterval(timerInterval);
-//             // showScores();
-//         }
-//     }, 1000);
-// }
-
-// function showScores() {
-//     "My High Scores!"
-// }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# javascript-quiz
+# **JavaScript Quiz**
+
+## **Project Overview**
+
+For this project, I wrote a JavaScript Quiz game utilizing JavaScript, HTML, and CSS. The game is timed and allows the user to click through multiple choice questions, keep track of the number of correct answers and deducting 10 seconds for each incorrect answer. Once complete, the user enters their initials and view their score.
+
+This project introduced me to Web APIs, the DOM, styling and setting element attributes from JS, intervals and timers, event listeners, local storage, and data attributes.
+
+## **Installation**
+
+N/A
+
+## **Usage**
+
+The game enables the user to review basic information about JavaScript.
+
+## **Credits**
+
+No starter code was provided.
+Alistair Rowden, Sergio Garcia, and Diem Ly all provided tutoring and guidance.
+
+## **License**
+
+Please refer to the license in the repo.
